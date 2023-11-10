68a0666a45e1059844056fd6b62a35484670accd
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-11-10 15:03:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,17 +10,12 @@ void generateEPC(char* epc) {
 }
 
 int main() {
-    // Seed for the random number generator
+
     srand((unsigned int)time(NULL));
 
-    // Number of EPCs to generate
-    int numEPCs = 5;
-
-    for (int i = 0; i < numEPCs; ++i) {
-        char epc[25];  // Each byte represented by two characters, plus a null terminator
-        generateEPC(epc);
-        printf("EPC %d: %s\n", i + 1, epc);
-    }
+    char epc[25];  // Each byte represented by two characters, plus a null terminator
+    generateEPC(epc);
+    printf("%s\n", epc);
 
     return 0;
 }
