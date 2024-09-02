3c6425e2835eb25ea8da2917c70f5d5f0a20ab12
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-09-02 17:59:45 -0300

    Create main.c

---
+++ reader-simulator/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+void generateEPC(char* epc) {
+    for (int i = 0; i < 12; ++i) {
+        sprintf(epc + i * 2, "%02X", rand() % 256);
+    }
+}
+
+int main() {
+    while (1) {
+        struct timeval tv;
+
+        if (gettimeofday(&tv, NULL) == -1) {
+            return 1;
+        }
+
+        double time_in_mill = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+
+        srand((unsigned int)time_in_mill);
+
+        char epc[25];
+        generateEPC(epc);
+
+        // Construct the JSON
+        //printf("{\"id\":\"%s\",\"readingDate\":%.0f,\"antenna\":1}\n", epc, time_in_mill);
+        printf("{\"id\":\"1000000093\",\"readingDate\":%.0f,\"antenna\":1}\n", time_in_mill);
+
+        // TO-DO cURL :
+
+        sleep(5);
+        fflush(stdout);  // Flush the output
+    }
+
+    return 0;
+}
